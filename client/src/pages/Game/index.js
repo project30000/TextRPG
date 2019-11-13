57ca670fe017effd983e0d7b6463d52f9fd39cc8
Fahad <fahad011686@gmail.com>
2019-11-12 18:06:27 -0600

    Text posting to state from JSON

--- client/src/pages/Game/index.js
+++ client/src/pages/Game/index.js
@@ -4,11 +4,24 @@ import Typist from 'react-typist';
 import 'react-typist/dist/Typist.css';
 import './style.css';
 import dialog from './dialog.json';
+import Button from '../../components/Button';
+import Text from '../../components/Text';
+
 
 class Game extends Component {
     state = {
-        dude: ""
+        dude: "", //user logged in
+        data: dialog,
+        curOptions: dialog[1]
+        
+
     }
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        console.log("Button clicked!");
+        console.log(document.getElementById('optionButtons').value);
+    };
 
     componentDidMount() {
         API.getMyDude(this.props.username).then(myDude => {
@@ -16,77 +29,71 @@ class Game extends Component {
                 dude: myDude
             })
         })
+
         // this.startGame();
     }
 
+
+    // const imageStyle = {
+    //     width: 400
+    // }
+
     render() {
-        // const imageStyle = {
-        //     width: 400
-        // }
-
-        const textElement = document.getElementById('text');
-        const optionButtonsElement = document.getElementById('optionButtons');
-
         const textNodes = dialog;
+
+
 
         const showTextNode = (textNodeIndex) => {
             const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
-            textElement.innerText = textNode.text
-            while (optionButtonsElement.firstChild) {
-                optionButtonsElement.removeChild(optionButtonsElement.firstChild)
-            }
-
-            textNode.options.forEach(option => {
-                if (showOption(option)) {
-                    const button = document.createElement('button')
-                    button.innerText = option.text
-                    button.classList.add('btn')
-                    button.addEventListener('click', () => selectOption(option))
-                    optionButtonsElement.appendChild(button)
-                }
-            })
+            // textElement.innerText = textNode.text
+            // while (optionButtonsElement.firstChild) {
+            //     optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+            // }
+            // this.state.setState({ curOptions: textNode.options })
         }
 
-        const showOption = (option) => {
-            return option.requiredState == null
-        }
+        // const showOption = (option) => {
+        //     return option.requiredState == null
+        // }
 
-        const selectOption = (option) => {
-            const nextTextNodeId = option.nextText
-            if (nextTextNodeId <= 0) {
-                return startGame()
-            }
-            showTextNode(nextTextNodeId)
-        }
+        // const selectOption = (option) => {
+        //     const nextTextNodeId = option.nextText
+        //     if (nextTextNodeId <= 0) {
+        //         // return startGame()
+        //     }
+        //     showTextNode(nextTextNodeId)
+        // }
 
-        const startGame = () => {
-            showTextNode(1)
-        }
+        // const startGame = () => {
+        //     showTextNode(1)
+        // }
 
         return (
             <div>
                 <br />
                 <br />
 
-                <div className="container">
-                    {console.log(textNodes)}
-                    <Typist>
-                        <p id="text"> You're awake. </p>
-                        <Typist.Delay ms={1000} />
+                {/* {console.log(textNodes[0])} */}
 
-                        You must have been asleep for weeks.
 
-                    </Typist>
-                    <br />
-                    <div id="optionButtons" className="btn-grid">
-                        <button className="btn">Option 1</button>
-                        <button className="btn">Option 2</button>
-                        <br />
-                        <button className="btn">Option 3</button>
-                        <button className="btn">Option 4</button>
-                    </div>
-                </div>
-            </div>
+
+                < Text
+                    text={this.state.curOptions.text}
+                />
+
+
+            <br />
+                {console.log("Check: " + this.state.curOptions)}
+                < div >
+                    {
+                        this.state.curOptions.options.map(option => (
+                            < Button
+                                onClick={this.handleFormSubmit}
+                                option={option.text}
+                            />))
+                    }
+                </div >
+            </div >
         )
 
     }
