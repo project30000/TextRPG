eb038d3cd21d4d0ef42f30db14aa8d52528cfbfc
Fahad <fahad011686@gmail.com>
2019-11-15 12:39:58 -0600

    Co-authored-by: Kemp

--- client/src/pages/Game/index.js
+++ client/src/pages/Game/index.js
@@ -12,9 +12,10 @@ class Game extends Component {
         userID: "5dca461355fad0bd7116f38a", //user logged in
         dialog: dialog,
         currentLine: dialog[0] //game init at first line
+
     }
 
-    handleClick = (nextText,op, end) => {
+    handleClick = (nextText, op, end) => {
         if (op) {
             this.props.incrementDeath(this.props.data.killCount);
         }
@@ -32,13 +33,13 @@ class Game extends Component {
         API.getMyDude(this.props.username).then(myDude => {
             // this.setState({
             //     userID: myDude
-                
+
             // })
         })
-        console.log(this.state.userID)
+        console.log("killcount ==========" + this.props.data.killCount)
         this.setState({ currentLine: dialog[0] })
         this.props.finishGame(this.state.userID)
-       
+
         // console.log(this.state.currentLine.options)
         // console.log(dialog[0])
         // this.startGame();
@@ -50,30 +51,32 @@ class Game extends Component {
     // }
 
     render() {
-        var nextOptions =this.state.currentLine.options.filter(e => e.killcount <= this.props.data.killCount)
+        var nextOptions = this.state.currentLine.options.filter(e => e.killcount === this.props.data.killCount)
         return (
             <div>
                 <br />
-                <br />
-                < Text text={this.state.currentLine.text} />
-                <br />
-                {
-                    
-                    console.log(nextOptions)
-                }
-                {
-                    nextOptions.map(option => (
-                        // console.log(option.nextText)
-                        // if (option.killcount > this.state.killCount)
-                        < Button
-                            option={option.text}
-                            nextText={option.nextText}
-                            handleClick={this.handleClick.bind("click", option.nextText, option.kill,option.end)}
-                        />
-                    ))
-                }
-                
+                <div class="container">
+                    <br />
+                    < Text text={this.state.currentLine.text} />
+                    <br />
+                    {
 
+                        console.log(nextOptions)
+                    }
+                    {
+                        nextOptions.map(option => (
+                            // console.log(option.nextText)
+                            // if (option.killcount > this.state.killCount)
+                            < Button
+                                option={option.text}
+                                nextText={option.nextText}
+                                handleClick={this.handleClick.bind("click", option.nextText, option.kill, option.end)}
+                            />
+                        ))
+                    }
+                    <br />
+                </div>
+                <br />
             </div >
         )
     }
