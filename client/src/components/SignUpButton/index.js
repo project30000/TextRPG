79f45162636e547a2e925417b353a5dc7d80de17
Alojzy Klim <adklim@gmail.com>
2019-11-15 11:24:07 -0600

    updated package for shake feature

--- client/src/components/SignUpButton/index.js
+++ client/src/components/SignUpButton/index.js
@@ -1,8 +1,30 @@
 import React, { component } from 'react';
+import { Shake } from 'reshake'
+ 
 
-const SignUpButton = props = () => {
-    <button>SignUpButton</button>
+const SignUp = props = () => {
+    <ShakeRotate >
+    <button>Click Me!</button>
+    </ShakeRotate>
 }
-<div class="shake-chunk"></div>
 
-export default SignUpButton
+class App extends Component {
+    render () {
+      return (
+        <Shake 
+          h={5}
+          v={5}
+          r={3}
+          dur={300}
+          int={10}
+          max={100}
+          fixed={true}
+          fixedStop={false}
+          freez={false}>
+          <h1>&lt;Shake /&gt;</h1>
+        </Shake>
+      )
+    }
+  }
+
+export default SignUp
