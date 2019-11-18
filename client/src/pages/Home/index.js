94a62ab68d3227454976902d71d7854b97f6e6d0
unknown <sternwillct@gmail.com>
2019-11-18 01:47:17 -0600

    Final stages

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -3,16 +3,16 @@ import axios from 'axios'
 
 
 class Home extends Component {
-    
-        constructor() {
-            super()
-            this.state = {
-      
-            }
-    
+
+    constructor() {
+        super()
+        this.state = {
+
         }
-    
-     
+
+    }
+
+
 
     render() {
         const imageStyle = {
@@ -22,77 +22,92 @@ class Home extends Component {
                 {/* <div>
             <audio ref="audio_tag" src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" controls autoPlay/>
  </div> */}
-            <div class="parallax">
-                <div id="group1" class="parallax__group">
-                    <div class="parallax__layer parallax__layer--base">
-                    {/* <img src="https://i.ibb.co/ck8GYj4/renderbuilding1.png"></img> */}
-                    {/* <img src="https://i.ibb.co/Nm3kJJM/picture1.png"></img> */}
-                    {/* <img src="https://i.ibb.co/XxYV7pB/picturebackground2.jpg"></img>   */}
-                    <img src="https://live.staticflickr.com/65535/49081592552_b8fc4b0b36_k.jpg"></img>  
-
-                     </div>
-                </div>
-                <div id="group2" class="parallax__group">
-                    <div class="parallax__layer parallax__layer--base">
-                        <div class="title">
-                            <h1>Wake up.....</h1>
-                            <hr></hr>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </div>
-                    </div>
-                    <div class="parallax__layer parallax__layer--back">
-                    </div>
-                </div>
-                <div id="group3" class="parallax__group">
-                    <div class="parallax__layer parallax__layer--fore">
-                        <div class="title">
-                        </div>
-                    </div>
-                    <div class="parallax__layer parallax__layer--base">
-                        <div class="title"></div>
-                    <img src="https://live.staticflickr.com/65535/49081549446_9e74d07c10_k.jpg"></img>
-                    </div>
-                </div>
-                <div id="group4" class="parallax__group">
-                    <div class="parallax__layer parallax__layer--base">
-                                    </div>
-                    <div class="parallax__layer parallax__layer--back">                        
-                        <h1>Are you ready to wake up?</h1>
-                            <hr></hr>
-                        <p></p>
-                    </div>
-                    <div class="parallax__layer parallax__layer--deep">
-                    </div>
-                </div>
-                <div id="group5" class="parallax__group">
-                    <div class="parallax__layer parallax__layer--fore">
-                    <img src ="https://live.staticflickr.com/65535/49081693542_70757cb03e_k.jpg"></img>
-                    </div>
-                    <div class="parallax__layer parallax__layer--base">
-                        <div class="title">
-                        {/* <img src="https://i.ibb.co/52n5DP9/background-image-3.jpg"></img> */}
+                <div class="parallax">
+                    <div id="group1" class="parallax__group">
+                        <div class="parallax__layer parallax__layer--base">
+                            {/* <img src="https://i.ibb.co/ck8GYj4/renderbuilding1.png"></img> */}
+                            {/* <img src="https://i.ibb.co/Nm3kJJM/picture1.png"></img> */}
+                            {/* <img src="https://i.ibb.co/XxYV7pB/picturebackground2.jpg"></img>   */}
+                            <img src="https://live.staticflickr.com/65535/49081592552_b8fc4b0b36_k.jpg"></img>
 
                         </div>
                     </div>
-                </div>
-                {/* <div id="group6" class="parallax__group">
+                    <div id="group2" class="parallax__group">
+                        <div class="parallax__layer parallax__layer--base">
+                            <div class="title">
+                                <h1>Wake up.....</h1>
+                                <hr></hr>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </div>
+                        <div class="parallax__layer parallax__layer--back">
+                        </div>
+                    </div>
+                    <div id="group3" class="parallax__group">
+                        <div class="parallax__layer parallax__layer--fore">
+                            <div class="title">
+
+                            </div>
+                        </div>
+                        <div class="parallax__layer parallax__layer--base">
+                            <div class="title"></div>
+                            <img src="https://live.staticflickr.com/65535/49081549446_9e74d07c10_k.jpg"></img>
+                        </div>
+                    </div>
+                    <div id="group4" class="parallax__group">
+                        <div class="parallax__layer parallax__layer--base">
+                        </div>
+                        <div class="parallax__layer parallax__layer--back">
+
+
+                            <h1>Are you ready to wake up?</h1>
+                            <hr></hr>
+                            <p>Ready to take on the day through the choices you make?</p>
+                            <br></br>
+                            <button type="submit" formaction="/login">Login</button><hr></hr>
+
+                            <button formaction="/sign-up">Sign Up</button>
+
+                        </div>
+                        <div class="parallax__layer parallax__layer--deep">
+                        </div>
+                    </div>
+                    <div id="group5" class="parallax__group">
+                        <div class="parallax__layer parallax__layer--fore">
+                            <img src="https://live.staticflickr.com/65535/49081693542_70757cb03e_k.jpg"></img>
+                        </div>
+                        <div class="parallax__layer parallax__layer--base">
+                            <div class="title">
+                                {/* <img src="https://i.ibb.co/52n5DP9/background-image-3.jpg"></img> */}
+
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div id="group6" class="parallax__group">
                     <div class="parallax__layer parallax__layer--back"> */}
                     {/* <img src="https://i.ibb.co/Nm3kJJM/picture1.png"></img> */}
 
                     {/* </div>
                     <div class="parallax__layer parallax__layer--base"> */}
                     {/* <img src="https://i.ibb.co/nzw417m/Untitled-333.png"></img>  */}
-                        {/* <div class="title"></div>
+                    {/* <div class="title"></div>
                     </div>
                 </div> */}
-                <div id="group7" class="parallax__group">
-                    <div class="parallax__layer parallax__layer--base">
-                        <div class="title">Footer</div>
+                    <div id="group7" class="parallax__group">
+                        <div class="parallax__layer parallax__layer--base">
+                            <div class="title">
+                                <pl>Project by Fahad, Al, Kemp and will.</pl>
+                                <ul>
+                                    <li><a href="https://twitter.com/">Twitter</a></li>
+                                    <li><a href="facebook.com">Facebook</a></li>
+                                    <li><a href="mailto:digitalcrab@digitalcrab.com">Email</a></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
-             )
+        )
 
     }
 }
