0e61aa0236fb987fa59d4681812e2773f184d148
Kemp Atkinson <kempatkinson@gmail.com>
2019-12-05 17:21:47 -0600

    display text based on ending choice

--- client/src/components/Stats/index.js
+++ client/src/components/Stats/index.js
@@ -1,10 +1,18 @@
 import React, { component } from 'react';
 
+const endings = [
+    // "You handled this nonviolently! Congrats. Some would call you Ghandi. Too bad you are stuck in the room.", "You almost got it, but you didn't attack enough times. Nice try.", "Violence is always the answer! Congrats on escaping."
+    " passive stuck",  
+    " aggresive stuck", 
+    "paradox escape",
+    "unplug escape" ,
+]
 
 const Stats = props => (
     <div>
-        <p className="yourScore">Your score: {props.data.killCount}</p>
-        <p className="avgScore">User averages: {props.data.userAverage}</p>
+        {/* <p className="yourScore">Your score: {props.data.killCount} </p> */}
+        <p> {endings[props.data.killCount]} </p>
+        {/* <p className="avgScore">User averages: {props.data.userAverage}</p> */}
     </div>
 
 )
